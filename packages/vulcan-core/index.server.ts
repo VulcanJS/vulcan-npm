d69b75c112728ffbc1383289c05c121e66715c80
eric-burel <eb@lebrun-burel.com>
2020-02-26 19:33:54 +0100

    lerna able to run tsc but hackish

--- packages/vulcan-core/index.server.ts
+++ packages/vulcan-core/index.server.ts
@@ -1,3 +1,3 @@
 // reexport users functions
-export * from '@vulcan/users'
+export * from '@vulcan/users/index.server' // TODO: we should not need the "index.client" part! Needs Webpack when building the code
 export * from './common'
