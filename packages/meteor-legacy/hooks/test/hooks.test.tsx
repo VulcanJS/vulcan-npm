e852e367b8a8b946e20cf740a210e4a84473ce41
eric-burel <eb@lebrun-burel.com>
2020-09-30 10:39:40 +0200

    add dummy test for hooks

---
+++ packages/meteor-legacy/hooks/test/hooks.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import expect from "expect";
+import { mount } from "enzyme";
+//import gql from 'graphql-tag';
+//import { initComponentTest } from 'meteor/vulcan:test';
+import {
+  useCurrentUser,
+  useLogout,
+  useAuthenticateWithPassword,
+  useSignup,
+} from "../index";
+
+describe("meteor-legacy/hooks", function () {
+  describe("exports", () => {
+    test("export relevant hooks", () => {
+      expect(useCurrentUser).toBeInstanceOf(Function);
+      expect(useLogout).toBeInstanceOf(Function);
+      expect(useSignup).toBeInstanceOf(Function);
+      expect(useAuthenticateWithPassword).toBeInstanceOf(Function);
+    });
+  });
+});
