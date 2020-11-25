66fc905cf52e20e94766af8d0bb4290f8b206055
eric-burel <eb@lebrun-burel.com>
2020-11-25 18:09:14 +0100

    make mongoose a peer dependency

--- scripts/link-duplicates.sh
+++ scripts/link-duplicates.sh
@@ -2,6 +2,8 @@
 
 # Avoid duplicate versions of React in root project by forcing the Vulcan NPM version
 # @see https://github.com/VulcanJS/vulcan-npm/issues/5
-# => this is avoided by using peerDependencies
+# This guarantees we use the same peer dependencies when running the packages
 cd ./node_modules/react && yarn link && cd ../..
 cd ./node_modules/react-dom && yarn link && cd ../..
+cd ./node_modules/@apollo/client && yarn link && cd ../..
+cd ./node_modules/mongoose && yarn link && cd ../..
