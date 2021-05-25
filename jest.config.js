c51386ae32357ca34af7412bb09d48a9ae5c4ce6
eric-burel <eb@lebrun-burel.com>
2021-05-25 16:20:12 +0200

    prevent jest projects duplication

--- jest.config.js
+++ jest.config.js
@@ -113,7 +113,7 @@ module.exports = {
   // preset: undefined,
 
   // Run tests from one or more projects
-  projects: ["<rootDir>/packages/*"],
+  // projects: ["<rootDir>/packages/*"],
 
   // Use this configuration option to add custom reporters to Jest
   // reporters: undefined,
