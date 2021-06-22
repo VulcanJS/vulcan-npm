a5ea94653fae6a4727ce8959cfa13513e69815ea
eric-burel <eb@lebrun-burel.com>
2021-06-22 19:35:44 +0200

    update comments

--- packages/mdx/webpack.config.js
+++ packages/mdx/webpack.config.js
@@ -1,12 +1,13 @@
-// This is a server-only package (currently)
 const merge = require("webpack-merge");
 const path = require("path");
+// This is a server-only package (currently)
 const baseConfig = require("../../webpack/webpack.config.base.server.prod");
 //const merge = require('webpack-merge')
 module.exports = merge(baseConfig, {
   entry: path.resolve(__dirname, "./index.ts"),
   output: {
     path: path.resolve(__dirname, "dist"),
+    // do not forget to add this otherwise the file will be named index.server.js
     filename: "index.js",
   },
 });
