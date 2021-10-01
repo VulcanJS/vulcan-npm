b1f20736a2af747131f309c56f522f0916a0f8ba
eric-burel <eb@lebrun-burel.com>
2021-10-01 12:22:59 +0200

    use the typesVersions trick to allow multiple entry points

--- packages/mongo/webpack.config.client.js
+++ packages/mongo/webpack.config.client.js
@@ -6,6 +6,6 @@ module.exports = merge(baseConfig, {
   entry: path.resolve(__dirname, "./client/index.ts"),
   output: {
     // NOTE: @vulcanjs/mongo is a server-first package, so to get isomorphic code you need to import @vulcanjs/mongo/client
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist/client"),
   },
 });
