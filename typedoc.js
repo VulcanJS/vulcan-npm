889bb12f2fe53c6a6d3828bf3cb8a530a959944e
eric-burel <eb@lebrun-burel.com>
2021-11-15 09:48:57 +0100

    use a config file for typedoc

---
+++ typedoc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entryPoints: [
+    "packages/graphql/index.ts",
+    "packages/graphql/server/index.ts",
+    "packages/i18n/index.ts",
+    "packages/mdx/index.ts",
+    "packages/meteor-legacy/index.ts",
+    "packages/model/index.ts",
+    "packages/mongo/index.ts",
+    "packages/mongo/client/index.ts",
+    "packages/permissions/index.ts",
+    "packages/react-hooks/index.ts",
+    "packages/react-ui/index.ts",
+    "packages/schema/index.ts",
+    "packages/utils/index.ts",
+  ],
+  out: "generated/docs",
+};
