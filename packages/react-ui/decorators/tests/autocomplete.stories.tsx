3046b9fbf83afe6fb452f005c25ab1914b21e5a7
eric-burel <eb@lebrun-burel.com>
2021-12-02 16:06:38 +0100

    demo for autocomplete component

--- packages/react-ui/decorators/tests/autocomplete.stories.tsx
+++ packages/react-ui/decorators/tests/autocomplete.stories.tsx
@@ -1,24 +1,137 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { createGraphqlModel } from "@vulcanjs/graphql";
+import { makeAutocomplete } from "../autocomplete";
+import {
+  Form,
+  FormProps,
+  VulcanComponentsProvider,
+} from "../../components/form";
+
+const people = createGraphqlModel({
+  name: "People",
+  graphql: {
+    typeName: "People",
+    multiTypeName: "Peoples",
+  },
+  schema: {},
+});
+
+const perms = {
+  canRead: ["guests"],
+  canCreate: ["guests"],
+};
+const autocompleteModel = createGraphqlModel({
+  name: "AutocompleteModel",
+  graphql: {
+    typeName: "Autocomplete",
+    multiTypeName: "Autocompletes",
+  },
+  schema: {
+    project: makeAutocomplete(
+      // the field
+      {
+        ...perms,
+        suffix: "prenormalized",
+        type: Array,
+        arrayItem: {
+          type: String,
+          optional: true,
+        },
+      },
+      // the decorator options
+
+      {
+        // TODO: we will probably need to update those params
+        autocompletePropertyName: "name",
+        queryResolverName: "projects",
+        fragmentName: "ProjectFragment",
+        valuePropertyName: "id",
+      }
+    ),
+    people: makeAutocomplete(
+      {
+        ...perms,
+        suffix: "prenormalized",
+        type: Array,
+        arrayItem: {
+          type: String,
+          optional: true,
+        },
+        options: (props) => {
+          return props?.data?.entities.map((document) => ({
+            ...document,
+            value: document.id,
+            label: document.name,
+          }));
+        },
+        query: () => /* GraphQL */ `
+          query FormComponentDynamicEntityQuery($value: [String!]) {
+            entities(id: { _in: $value }) {
+              id
+              name
+            }
+          }
+        `,
+        autocompleteQuery: () => /* GraphQL */ `
+          query AutocompletePeopleQuery($queryString: String) {
+            entities(tags: ["people"], name: { _like: $queryString }) {
+              id
+              name
+            }
+          }
+        `,
+      },
+      {
+        // TODO: we will probably need to update those params
+        autocompletePropertyName: "name", // overridden by field definition above
+        queryResolverName: "entities", // overridden by field definition above
+        fragmentName: "EntityFragment", // overridden by field definition above
+        valuePropertyName: "id", // overridden by field definition above
+      }
+    ),
+  },
+});
 
 //import { AutocompleteDemo, AutocompleteDemoProps } from '../AutocompleteDemo'
-interface AutocompleteDemoProps {}
+interface AutocompleteDemoProps extends FormProps {}
 const AutocompleteDemo = (props: AutocompleteDemoProps) => {
   // TODO: create a model + smart form with autcomplete feature
-  return <>"TODO"</>;
+  return <Form {...props} />;
 };
 export default {
   component: AutocompleteDemo,
   title: "AutocompleteDemo",
   decorators: [
     (Story) => (
-      <div>
+      <VulcanComponentsProvider>
         <Story />
-      </div>
+      </VulcanComponentsProvider>
     ),
   ],
-  args: {},
+  //args: {},
+  //parameters: { actions: { argTypesRegex: "^.*Callback$" } },
+  // Form props
+  args: {
+    model: autocompleteModel,
+    createDocument: (variables) => {
+      action("createDocument");
+      return Promise.resolve({ document: variables.input.data, errors: [] });
+    },
+    updateDocument: (variables) => {
+      action("updateDocument");
+      return Promise.resolve({ document: variables.input.data, errors: [] });
+    },
+  },
+  argTypes: {
+    // createDocument: { action: "createDocument" },
+    // updateDocument: { action: "updateDocument" },
+    deleteDocument: { action: "deleteDocument" },
+  },
   parameters: { actions: { argTypesRegex: "^.*Callback$" } },
+  // another syntax for actions
+  //} as Meta<FormProps>;
 } as Meta<AutocompleteDemoProps>;
 
 const AutocompleteDemoTemplate: Story<AutocompleteDemoProps> = (args) => (
