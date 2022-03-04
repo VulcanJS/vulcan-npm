5cb925adad04563b4affd313965d8109f418fc49
eric-burel <eb@lebrun-burel.com>
2022-03-04 17:20:28 +0100

    move more packages to esbuild

---
+++ packages/react-hooks/tsup.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "tsup";
+import path from "path";
+
+const commonConfig = {
+  clean: true,
+  splitting: false,
+  // Skip until .d.ts.map is also supported https://github.com/egoist/tsup/issues/564
+  // dts: true,
+  sourcemap: true,
+  tsconfig: path.resolve(__dirname, "./tsconfig.build.json"),
+};
+export default defineConfig([
+  {
+    entry: ["index.ts"],
+    ...commonConfig,
+    // IMPORTANT: keep iife around so that CommonJS ends with .cjs
+    format: ["cjs", "esm", "iife"],
+    outDir: "dist",
+  },
+  /*
+  No client-specific code yet
+  {
+    entry: ["client/index.ts"],
+    ...commonConfig,
+    format: ["esm", "iife"],
+    outDir: "dist/client",
+  },*/
+]);
---
+++ packages/react-ui-material/tsup.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "tsup";
+import path from "path";
+
+const commonConfig = {
+  clean: true,
+  splitting: false,
+  // Skip until .d.ts.map is also supported https://github.com/egoist/tsup/issues/564
+  // dts: true,
+  sourcemap: true,
+  tsconfig: path.resolve(__dirname, "./tsconfig.build.json"),
+};
+export default defineConfig([
+  {
+    entry: ["index.ts"],
+    ...commonConfig,
+    // IMPORTANT: keep iife around so that CommonJS ends with .cjs
+    format: ["cjs", "esm", "iife"],
+    outDir: "dist",
+  },
+  /*
+  There is no server/client specific code in this package yet,
+  every component is isomorphic (work on client and server)
+  {
+    entry: ["server/index.ts"],
+    ...commonConfig,
+    format: ["cjs"],
+    outDir: "dist/server",
+  },
+  {
+    entry: ["client/index.ts"],
+    ...commonConfig,
+    format: ["esm", "iife"],
+    outDir: "dist/client",
+  },*/
+]);
