8f88426aae2fffde6732d513d6aa999349d46767
eric-burel <eb@lebrun-burel.com>
2022-05-04 13:39:52 +0200

    init remix stack based on indi stack and publish to read-only repos

--- scripts/push-starters.mjs
+++ scripts/push-starters.mjs
@@ -18,3 +18,12 @@ const nextRepo = "https://github.com/VulcanJS/vulcan-next.git";
 
 await $`git subtree push ${nextRepo} main --prefix ${nextPrefix}`;
 console.info("Pushed Vulcan Next to read-only starter repository");
+
+const remixPrefix = `${startersFolder}/remix`;
+const remixRepo = "https://github.com/VulcanJS/vulcan-remix.git";
+// just an alias to respect Remix terminology
+const remixRepoEurodance = "https://github.com/VulcanJS/eurodance-stack.git";
+
+await $`git subtree push ${remixRepo} main --prefix ${remixPrefix}`;
+await $`git subtree push ${remixRepoEurodance} main --prefix ${remixPrefix}`;
+console.info("Pushed Vulcan Remix to read-only starter repository");
