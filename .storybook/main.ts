3a77989174104d694a07767ee309dca06be9b835
eric-burel <eb@lebrun-burel.com>
2022-05-04 13:26:22 +0200

    ts sb

---
+++ .storybook/main.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    // specifying packages folder is important to avoid rebuilding on cache change
+    // @see https://github.com/storybookjs/storybook/issues/14342
+    "../packages/**/*.stories.mdx",
+    "../packages/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  staticDirs: ["./public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+};
