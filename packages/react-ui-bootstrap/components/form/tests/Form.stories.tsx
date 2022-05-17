76fb9f7ccb5a03bbe6bf86e29a0dca7af290b19f
eric-burel <eb@lebrun-burel.com>
2022-05-17 15:59:37 +0200

    add style to form story

--- packages/react-ui-bootstrap/components/form/tests/Form.stories.tsx
+++ packages/react-ui-bootstrap/components/form/tests/Form.stories.tsx
@@ -20,6 +20,15 @@ export default {
   decorators: [
     (Story) => (
       <VulcanComponentsProvider value={bootstrapVulcanComponents}>
+        {/** Hacky solution to get styling, until Storybook
+         * can load a config per package
+         */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossOrigin="anonymous"
+        />
         <Story />
       </VulcanComponentsProvider>
     ),
