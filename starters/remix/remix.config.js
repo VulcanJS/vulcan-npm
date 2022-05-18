fd68925d46f09ac8d7ca9f725b9f62a9cf2729f4
eric-burel <eb@lebrun-burel.com>
2022-05-18 17:04:41 +0200

    setup vercel only server

--- starters/remix/remix.config.js
+++ starters/remix/remix.config.js
@@ -7,7 +7,22 @@ module.exports = {
   /**
    * Remove if not using Vercel
    * @see https://remix.run/docs/en/v1/api/conventions#serverbuildtarget
-   * @see https://github.com/robipop22/dnb-stack/blob/main/remix.config.js
    * */
   serverBuildTarget: "vercel",
+  /**
+   * Remove if not using Vercel
+   *
+   * Inspiredf from DNB stack.
+   *
+   * @example
+   * yarn run dev => remix dev server
+   * yarn run build => use remix prod server, locally
+   * VERCEL=1 yarn run build => use vercel server, locally
+   * On Vercel: => yarn run build will use vercel server (VERCEL is automatically set)
+   *
+   * @see https://vercel.com/docs/concepts/projects/environment-variables
+   *
+   * @see https://github.com/robipop22/dnb-stack/blob/main/remix.config.js
+   */
+  server: process.env.VERCEL ? "./vercel.server.js" : undefined,
 };
