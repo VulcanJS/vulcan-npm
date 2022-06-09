9a2c6bb543227d9a3c4c14dbc4e832da13a3090e
eric-burel <eb@lebrun-burel.com>
2022-06-09 18:21:40 +0200

    add pull command

--- scripts/push-next.mjs
+++ scripts/push-next.mjs
@@ -8,3 +8,8 @@ const nextRepo = "https://github.com/VulcanJS/vulcan-next.git";
 
 await $`git subtree push ${nextRepo} main --prefix ${nextPrefix}`;
 console.info("Pushed Vulcan Next to read-only starter repository");
+
+console.info(
+  "If you need to pull first, use",
+  `git subtree pull ${nextRepo} main --prefix ${nextPrefix}`
+);
