ca4a4dec18c4fcc59f9c28bd568b0124dff27f97
eric-burel <eb@lebrun-burel.com>
2022-06-20 11:55:40 +0200

    expose the typename string

--- packages/mongo-apollo/objectIdScalar.ts
+++ packages/mongo-apollo/objectIdScalar.ts
@@ -1,11 +1,17 @@
+// @see https://github.com/CaptorAB/graphql-objectid-scalar
 import { GraphQLObjectId } from "graphql-objectid-scalar";
 
 /**
  * NOTE: the type is ObjectID to be consistent with GraphQL "ID" type
  * Be careful with the casing
  */
-export const objectIdTypeDefs = `scalar ObjectId`;
+export const objectIdTypeDefs = `scalar GraphQLObjectId`;
 
 export const objectIdResolvers = {
   ObjectId: GraphQLObjectId,
 };
+
+/**
+ * GraphQL type for a Mongo ObjectId
+ */
+export const GraphqlObjectId = "GraphQLObjectId";
