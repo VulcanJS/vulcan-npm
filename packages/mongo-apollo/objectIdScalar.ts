373cbea2518f8fdaed14f2d3993f699887e1f943
eric-burel <eb@lebrun-burel.com>
2022-06-20 10:51:12 +0200

    introduce objectId scalar for mongo + graphql

---
+++ packages/mongo-apollo/objectIdScalar.ts
@@ -0,0 +1,11 @@
+import { GraphQLObjectId } from "graphql-objectid-scalar";
+
+/**
+ * NOTE: the type is ObjectID to be consistent with GraphQL "ID" type
+ * Be careful with the casing
+ */
+export const mongoTypeDefs = `scalar ObjectId`;
+
+export const mongoResolvers = {
+  ObjectId: GraphQLObjectId,
+};
