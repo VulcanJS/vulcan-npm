7e43e2c24132710905135867d3cddd9403103bb4
eric-burel <eb@lebrun-burel.com>
2022-07-21 17:42:21 +0200

    load test from storybook/react-testing

--- starters/remix/app/components/tests/GithubButtons.test.tsx
+++ starters/remix/app/components/tests/GithubButtons.test.tsx
@@ -1,8 +1,16 @@
 import { render } from "@testing-library/react";
 import { GithubButtons } from "../GithubButtons";
+import { composeStories } from "@storybook/testing-react";
+import * as stories from "./GithubButtons.stories"; // import all stories from the stories file
+
+const { Basic } = composeStories(stories);
 
 test("Render GithubButtons", () => {
   render(<GithubButtons />);
+  expect(document.querySelector("section")).toBeDefined();
 });
 
-test.skip("Render GithubButtons from Storybook", () => {});
+test("Render GithubButtons from Storybook", () => {
+  render(<Basic />);
+  expect(document.querySelector("section")).toBeDefined();
+});
