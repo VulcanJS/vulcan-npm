f81be9aa6c54d98c2dc32c29407743db2b1fbe73
eric-burel <eb@lbke.fr>
2022-09-29 08:29:12 +0200

    improve react-ui-lite bundle config, add sideEffects false

--- packages/react-ui-lite/components/VulcanComponents/liteVulcanComponents/index.ts
+++ packages/react-ui-lite/components/VulcanComponents/liteVulcanComponents/index.ts
@@ -1,22 +1,4 @@
-export { liteFormComponents } from "./formComponents";
-export { liteDatatableComponents } from "./datatableComponents";
-export { liteCellComponents } from "./cellComponents";
-export { liteCoreComponents } from "./coreComponents";
-
-import { liteFormComponents } from "./formComponents";
-import { liteDatatableComponents } from "./datatableComponents";
-import { liteCellComponents } from "./cellComponents";
-import { liteCoreComponents } from "./coreComponents";
-
-/**
- * @deprecated 0.6.1
- * Use each type of components instead of loading all of them at once
- * This will avoid loading form and datatable components, that are very heavy, on every page
- * even when they are anot actually used
- */
-export const liteVulcanComponents = {
-  ...liteDatatableComponents,
-  ...liteCellComponents,
-  ...liteCoreComponents,
-  ...liteFormComponents,
-};
+export { liteFormComponents } from "./formComponents.js";
+export { liteDatatableComponents } from "./datatableComponents.js";
+export { liteCellComponents } from "./cellComponents.js";
+export { liteCoreComponents } from "./coreComponents.js";
--- packages/react-ui-lite/tsup.config.ts
+++ packages/react-ui-lite/tsup.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "tsup";
+import { defineConfig, Format } from "tsup";
 import path from "path";
 
 const commonConfig = {
@@ -8,38 +8,41 @@ const commonConfig = {
   // dts: true,
   sourcemap: true,
   tsconfig: path.resolve(__dirname, "./tsconfig.build.json"),
+  outDir: "dist",
+  format: ["esm" as Format],
 };
 export default defineConfig([
   // actual exposed modules = 1 per component
   {
-    // This is a demo with one component but you can use a glob here
-    entry: ["./components/core/*.tsx"],
-    //["./components/core/Loading.tsx"],
+    entry: [
+      "./components/core/!(index).ts?(x)",
+      "./components/VulcanComponents/liteVulcanComponents/!(index).ts?(x)",
+    ],
     ...commonConfig,
-    format: ["esm"],
-    // TODO: it's not respect automatically
-    outDir: "dist/components/core",
-  },
-  {
-    // This is a demo with one component but you can use a glob here
-    entry: ["./components/VulcanComponents/liteVulcanComponents/index.ts"],
-    //["./components/core/Loading.tsx"],
-    ...commonConfig,
-    format: ["esm"],
-    // TODO: it's not respect automatically
-    outDir: "dist/components/VulcanComponents/liteVulcanComponents",
+    // For debugging, will output ESbuild metafile
+    // metafile: true,
+    esbuildOptions(options, context) {
+      // the directory structure will be the same as the source
+      options.outbase = "./";
+    },
   },
   // index files to allow named imports
   // inspired by react-bootstrap structure
   {
-    entry: ["index.ts", "./components/core/index.ts"],
+    entry: [
+      "index.ts",
+      "./components/core/index.ts",
+      "./components/VulcanComponents/liteVulcanComponents/index.ts",
+    ],
     ...commonConfig,
+    esbuildOptions(options, context) {
+      // the directory structure will be the same as the source
+      options.outbase = "./";
+    },
     // index files must NOT be bundled!
     // it acts as a map towards bundled components
     // but never rebundles them
     bundle: false,
-    format: ["esm"],
-    outDir: "dist",
   },
   /*
   There is no server/client specific code in this package yet,
